df3745ea19976a039376e346a004761e02b00a47
Kat-Devs <katlegophele95@gmail.com>
2023-10-29 20:08:24 +0200

    0x03 test_utils pycodestyle edits

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,48 +7,35 @@ Test unit for utils.py
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
-from typing import (
-    Dict,
-    Union,
-    Any,
-    List,
-    Tuple,
-    Callable
-)
+from typing import Dict, Union, Any, List, Tuple, Callable
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''Tests nested map function'''
+    """Tests nested map function"""
 
     passing_test_cases = [
-        ({"a":1}, ("a",), 1),
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ]
 
     @parameterized.expand(passing_test_cases)
     def test_access_nested_map(
-        self,
-        nested_map: Dict,
-        path: Tuple,
-        expected: Any
+        self, nested_map: Dict, path: Tuple, expected: Any
     ) -> None:
-        '''Tests access_nested_map'''
-        self.assertEqual(access_nested_map(nested_map, path), expected
-    )
-        
-    keyErr_test_cases =  [
+        """Tests access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    keyErr_test_cases = [
         ({}, ("a",), KeyError),
-        ({"a":1}, ("a", "b"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ]
-    
+
     @parameterized.expand(keyErr_test_cases)
     def test_access_nested_map_exception(
-        self,
-        nested_map: Dict,
-        path: Tuple,
-        expected: Any
+        self, nested_map: Dict, path: Tuple, expected: Any
     ) -> None:
-        '''Tests access_nested_map exception'''
+        """Tests access_nested_map exception"""
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
 

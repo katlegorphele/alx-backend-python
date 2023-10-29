31d60ad740d17e9d6c4a4458ef7e94c4741c8b2f
Kat-Devs <katlegophele95@gmail.com>
2023-10-29 20:05:18 +0200

    0x03

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ complex types - list of floats
 """
 from typing import List
 
+
 def sum_list(input_list: list[float]) -> float:
     """
     Returns the sum of a list of floats
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,7 @@ and returns their sum as a float.
 """
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of a list of floats
